7b0f47237d50d42f545d66612daa85b4cf6cbbf4
Savya Vats <savyavats@gmail.com>
2026-07-19 19:14:16 -0400

    check

--- TwoSum.py
+++ TwoSum.py
@@ -3,6 +3,10 @@ class Solution(object):
     def twoSum(self, nums, target):
         dict1 = {}
         
+        for i in range(len(nums)):
+            comp = target - nums[i]
+
+            if comp 
 
         return []
         """ for i in range(len(nums)-1):
