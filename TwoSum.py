9b54eed3044fc1023f61d9defd946bcc5eb86720
Savya Vats <savyavats@gmail.com>
2026-07-19 18:48:50 -0400

    added original two sum

--- TwoSum.py
+++ TwoSum.py
@@ -1,1 +1,12 @@
-print("this is working")
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)-1):
+            for j in range(1, len(nums)):
+                if (i != j):
+                    if (nums[i] + nums[j] == target):
+                        return [i,j]
