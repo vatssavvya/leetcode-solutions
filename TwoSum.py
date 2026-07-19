8821c34a779b97f9df86dcf5e6e7647962570a4c
Savya Vats <savyavats@gmail.com>
2026-07-19 19:17:23 -0400

    testing

--- TwoSum.py
+++ TwoSum.py
@@ -2,11 +2,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
         dict1 = {}
-        
+
         for i in range(len(nums)):
             comp = target - nums[i]
+            comp_ind = dict1.get(comp)
 
-            if comp 
+            if comp_ind is not None:
+                return [i, comp_ind]
 
         return []
         """ for i in range(len(nums)-1):
