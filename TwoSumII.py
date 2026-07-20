abcfba60f3349f0092de4e5c94082ca631dfc25c
Savya Vats <savyavats@gmail.com>
2026-07-20 12:01:40 -0400

    solution 1

--- TwoSumII.py
+++ TwoSumII.py
@@ -3,4 +3,7 @@ class Solution(object):
         dict1 = {}
         for i in range(len(numbers)):
             comp = target - numbers[i]
-            
+            if comp in dict1:
+                return [comp.value() + 1, i+1]
+            dict1[numbers[i]] = i
+        return []
