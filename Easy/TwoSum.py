b4179e9eebd0315def8764d0ac41345899195809
Savya Vats <savyavats@gmail.com>
2026-07-22 13:10:17 -0400

    spacing

--- Easy/TwoSum.py
+++ Easy/TwoSum.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
         dict1 = {}
-
         for i in range(len(nums)):
             comp = target - nums[i]
             comp_ind = dict1.get(comp)
-
             if comp_ind is not None:
                 return [comp_ind, i]
             dict1[nums[i]] = i
