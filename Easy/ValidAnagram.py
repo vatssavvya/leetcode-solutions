73400ab4baf4e046b9c0f734037057cb1c0111fd
Savya Vats <savyavats@gmail.com>
2026-07-23 11:16:01 -0400

    init

--- Easy/ValidAnagram.py
+++ Easy/ValidAnagram.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
         dict1 = {}
-        
-        
+        dict2 = {}
+        for i in range(len(s)):
+            if s[i] in dict1:
