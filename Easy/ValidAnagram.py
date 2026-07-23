c8c52794808b3a698dc2711e19cfaba696430808
Savya Vats <savyavats@gmail.com>
2026-07-23 11:17:48 -0400

    2 seperate hash tables

--- Easy/ValidAnagram.py
+++ Easy/ValidAnagram.py
@@ -6,3 +6,11 @@ class Solution(object):
         dict2 = {}
         for i in range(len(s)):
             if s[i] in dict1:
+                dict1[s[i]] += 1
+            else:
+                dict1[s[i]] = 1
+            if t[i] in dict1:
+                dict2[t[i]] += 1
+            else:
+                dict2[t[i]] = 1
+        
