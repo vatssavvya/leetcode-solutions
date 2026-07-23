5fcbb88b5ad2e36a649b9be7b8b43f0d0b84fca7
Savya Vats <savyavats@gmail.com>
2026-07-23 11:09:53 -0400

    init hash table

--- Easy/ValidAnagram.py
+++ Easy/ValidAnagram.py
@@ -1,8 +1,5 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+        dict1 = {}
+        
         
