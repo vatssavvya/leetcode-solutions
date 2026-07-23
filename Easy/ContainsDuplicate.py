efe617f841070646bf7a7e59d2def5e4dd8965d2
Savya Vats <savyavats@gmail.com>
2026-07-23 12:54:27 -0400

    initial thought comment

--- Easy/ContainsDuplicate.py
+++ Easy/ContainsDuplicate.py
@@ -1,4 +1,4 @@
-#used the set as its the fastest solution, but deprioritize for the 
+#used the set as its the fastest solution, but deprioritize for the learning process
 class Solution(object):
     def containsDuplicate(self, nums):
        newNums = set(nums)
