4809b3fa1ce6fe3ea2331141f39b21f035b6a9cb
Savya Vats <savyavats@gmail.com>
2026-07-24 17:35:53 -0400

    hardcoded solution

--- Medium/GroupAnagrams.py
+++ Medium/GroupAnagrams.py
@@ -14,11 +14,21 @@ class Solution(object):
 #hard-coded solution
 class Solution(object):
     def groupAnagrams(self, strs):
+        dict1 = {}
         for i in range(len(strs)):
             charFreq = [0] * 26
             for char in strs[i]:
                 charFreq[char-97]+=1
-            
+            key = ""
+            for k in range(26):
+                if charFreq[k] > 0:
+                    key.append(char(i+97) + str(charFreq[k]))
+            if key not in dict1:
+                dict1[key] = []
+            dict1[key].append(strs[i])
+        return list(dict1.values())
+
+
         print("test")
 
 
