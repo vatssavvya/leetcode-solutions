646d2849918f6a83ba32c134414f43ae3d33fe9d
Savya Vats <savyavats@gmail.com>
2026-07-24 13:04:03 -0400

    finalized solution

--- Medium/GroupAnagrams.py
+++ Medium/GroupAnagrams.py
@@ -1,6 +1,12 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        finalized = []
+        dict1 = {}
         for i in range(len(strs)):
-            print("test")
+            key = "".join(sorted(strs[i]))
+            if key not in dict1:
+                dict1[key] = []
+            dict1[key].append(strs[i])
+        return list(dict1.values())
+
+            
         
