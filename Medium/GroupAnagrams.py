434e4d2f6470c32ce9fa37e819e1c026b9d0b290
Savya Vats <savyavats@gmail.com>
2026-07-24 12:48:06 -0400

    working on groupanagrams

--- Medium/GroupAnagrams.py
+++ Medium/GroupAnagrams.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+        finalized = []
+        for i in range(len(strs)):
+            print("test")
         
