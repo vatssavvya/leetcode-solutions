c536a9554739dbf086f6dc36c3a8a63e7368d16d
Savya Vats <savyavats@gmail.com>
2026-07-28 19:49:52 -0400

    keyvalue

--- Medium/TopKFrequentElements.py
+++ Medium/TopKFrequentElements.py
@@ -1,5 +1,11 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
         #will finish later before August
-        
-        print("test")
+        dict1 = {}
+        for i in nums:
+            if i not in nums:
+                dict1[i] = 0
+            dict1[i] += 1
+        max1 = 0
+        for key, value in dict1.items():
+           print(key, value)
