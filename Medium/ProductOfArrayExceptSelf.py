ba440c43cae4f9df506fc2e3dae70b4830dcac70
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-07-30 14:04:22 -0400

    finalized so,ution

--- Medium/ProductOfArrayExceptSelf.py
+++ Medium/ProductOfArrayExceptSelf.py
@@ -1,11 +1,16 @@
 #fastest solution
 class Solution(object):
     def productExceptSelf(self, nums):
-        leftArray = []
-        rightArray = []
+        answers = [0] * len(nums)
+        leftArray = [1] * len(nums)
+        rightArray = [1] * len(nums)
+        for i in range(1, len(nums)):
+            leftArray[i] = leftArray[i-1] * nums[i-1]
+        for i in range(len(nums) - 2, -1, -1):
+            rightArray[i] = rightArray[i + 1] * nums[i + 1]
         for i in range(len(nums)):
-            
-
+            answers[i] = leftArray[i] * rightArray[i]
+        return answers
 
 #hard coded solution
 """class Solution(object):
