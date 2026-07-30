e1d212e24b75bd7df08c822663d860c618005f57
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-07-30 13:55:45 -0400

    minor fix to hard coded solution

--- Medium/ProductOfArrayExceptSelf.py
+++ Medium/ProductOfArrayExceptSelf.py
@@ -9,9 +9,7 @@
         for i in range(len(nums)):
             total = 1
             for j in range(len(nums)):
-                if j == i:
-                    continue
-                else:
+                if j !=i:
                     total *= nums[j]
             answer.append(total)
         return answer """
