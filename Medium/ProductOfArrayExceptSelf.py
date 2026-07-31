2b717620b11efe3d5d10906c36fa3609b351dea6
Savya Vats <savyavats@gmail.com>
2026-07-31 12:25:38 -0400

    testing

--- Medium/ProductOfArrayExceptSelf.py
+++ Medium/ProductOfArrayExceptSelf.py
@@ -11,7 +11,20 @@ class Solution(object):
         for i in range(len(nums)):
             answers[i] = leftArray[i] * rightArray[i]
         return answers
-
+nums = [1, 2, 3, 4]
+answers = [0] * len(nums)
+        
+leftArray = [1] * len(nums)
+rightArray = [1] * len(nums)
+for i in range(1, len(nums)):
+            leftArray[i] = leftArray[i-1] * nums[i-1]
+for i in range(len(nums) - 2, -1, -1):
+            rightArray[i] = rightArray[i + 1] * nums[i + 1]
+for i in range(len(nums)):
+            answers[i] = leftArray[i] * rightArray[i]
+print(leftArray)
+print(rightArray)
+print(answers)
 #hard coded solution
 """class Solution(object):
     def productExceptSelf(self, nums):
